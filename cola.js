f0e9191e60ea5985bd7a4f5114195a4ddce7226e
MusicCoLa <98040120+MusicCoLa@users.noreply.github.com>
2022-01-19 19:08:39 +0200

    Update cola.js

--- cola.js
+++ cola.js
@@ -4,6 +4,6 @@ var friends = [
         name: '𝕮𝖔𝕷𝖆',
         user: 'CoLaMusic',
         url: 'CrazZy.Lifeee',
-        img: 'https://i.postimg.cc/8cXVh7bm/maah.jpg'
+        img: 'https://i.imgur.com/GuAxbX4.jpg'
     }
 ];
