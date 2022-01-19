32a1f4e259c459cdb3784a4c98dcaa00158ace66
MusicCoLa <98040120+MusicCoLa@users.noreply.github.com>
2022-01-19 18:20:45 +0200

    Add files via upload

---
+++ cola.js
@@ -0,0 +1,51 @@
+
+var friends = [
+    {
+        name: 'Tia Maah',
+        user: 'Maah',
+        url: 'sinistra16',
+        img: 'https://i.postimg.cc/8cXVh7bm/maah.jpg'
+    },
+    {
+        name: 'Mana Eloh',
+        user: 'Eloh',
+        url: 'ELoh',
+        img: 'https://i.postimg.cc/YqDggcSk/eloh.jpg'
+    },
+    {
+        name: 'Tia Paula',
+        user: 'Ana Paula',
+        url: 'anapauia',
+        img: 'https://i.postimg.cc/dVzyH8dx/paula.jpg'
+    },
+    {
+        name: 'Mana Anneh',
+        user: 'Anneh',
+        url: 'xAnnezinha',
+        img: 'https://i.postimg.cc/y6P0134D/anneh.jpg'
+    },
+    {
+        name: 'Tio Nicolas',
+        user: 'Nicolas',
+        url: 'iNicoLas',
+        img: 'https://i.postimg.cc/Pxt9f27f/and-img.jpg'
+    },
+    {
+        name: 'Mana Misty',
+        user: 'Mistynha',
+        url: 'Mistynha',
+        img: 'https://i.postimg.cc/7ZCznBCz/misty.jpg'
+    },
+    {
+        name: 'Prima DK',
+        user: 'DK',
+        url: 'DkoficiaLangeL',
+        img: 'https://i.postimg.cc/8CJYDWtW/dk.jpg'
+    },
+    {
+        name: 'Mana Fran',
+        user: 'Fran',
+        url: 'iFrancinny',
+        img: 'https://i.postimg.cc/VNtbVn7c/franciny.jpg'
+    }
+];
